835fa6bb849b87dc77d3218bf816f884818c822a
costin2121 <dobrecostindragos@gmail.com>
2023-08-04 11:23:52 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,84 @@
+import ytdl from "ytdl-core";
+import yts from "yt-search";
+import fs, { PathLike } from "fs";
+import path from "path";
+
+export default class Youtube {
+  async video(
+    url: string,
+    filter: ytdl.Filter,
+    quality: ytdl.ChooseFormatQuality = "highest",
+    outputDirectory?: string
+  ) {
+    if (!ytdl.validateURL(url)) throw new Error("Invalid video url");
+
+    let videoInfo = await yts({
+      videoId: url.split("v=")[1].split("&")[0],
+    });
+    let dl = ytdl(url, {
+      filter: filter,
+      quality: quality,
+    });
+
+    let ext = ["audioandvideo", "videoandaudio", "video", "videoonly"].includes(
+      filter.toString()
+    )
+      ? "mp4"
+      : "mp3";
+
+    if (outputDirectory != undefined && !fs.existsSync(outputDirectory))
+      fs.mkdirSync(outputDirectory);
+    console.log(videoInfo.title.match(/ \//));
+    console.log(videoInfo.title.match(/ \//g));
+    let stream = fs.createWriteStream(
+      path.join(
+        outputDirectory || "./",
+        `./${videoInfo.title.replaceAll("/", "-")}.${ext}`
+      )
+    );
+    dl.pipe(stream);
+  }
+
+  async playlist(
+    url: string,
+    filter: ytdl.Filter,
+    quality: ytdl.ChooseFormatQuality = "highest",
+    outputDirectory?: string
+  ) {
+    if (
+      !url.startsWith("https://youtube.com/playlist") &&
+      !url.includes("list=")
+    )
+      return;
+
+    let id = url.split("list=")[1].split("&")[0];
+
+    let playlist = await yts({
+      listId: id,
+    });
+
+    for (const video of playlist.videos) {
+      let videoInfo = await yts({
+        videoId: video.videoId,
+      });
+      await this.video(
+        videoInfo.url,
+        filter,
+        quality,
+        `${outputDirectory || "./"}/${playlist.title.replaceAll(/ \//g, "-")}`
+      );
+    }
+  }
+}
+
+let yt = new Youtube();
+
+async function m() {
+  await yt.playlist(
+    "https://www.youtube.com/playlist?list=PL2xsJowpDh_A2pG95Daju4kG0C1aDm8Ti",
+    "videoandaudio",
+    "highestvideo"
+  );
+}
+
+m();
